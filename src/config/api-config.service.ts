19af2f6d480b07c474fe126cbbb33e97279100af
Thanh Tung <tung@codelynx.io>
2021-11-09 00:37:12 +0700

    arrange code

--- src/config/api-config.service.ts
+++ src/config/api-config.service.ts
@@ -1,12 +1,11 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from '@nestjs/config'
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ApiConfigService {
-  constructor(private configService: ConfigService) { }
+  constructor(private configService: ConfigService) {}
 
   get isAuthEnabled(): boolean {
     return this.configService.get('AUTH_ENABLED') === 'true';
   }
-
 }
